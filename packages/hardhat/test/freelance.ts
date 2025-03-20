d88304463b0648d2c59754134c6bb6219e7b39cb
blockend-dev <162776165+blockend-dev@users.noreply.github.com>
2025-03-20 21:44:28 +0000

    test : job creation by employer

--- packages/hardhat/test/freelance.ts
+++ packages/hardhat/test/freelance.ts
@@ -3,33 +3,37 @@ import { ethers } from "hardhat";
 import { Freelance } from "../typechain-types"; // Import the generated type
 
 describe("FreelancerRegistration", function () {
+
+  const name = "John Doe";
+  const skills = "Web Development";
+  const country = "USA";
+  const gigTitle = "Build a Website";
+  const gigDesc = "I will build a responsive website for your business.";
+  const images = ["image1.png", "image2.png"];
+  const startingPrice = ethers.parseEther("0.1");
+
+
   let freelancerContract: Freelance;
-  let owner;
+  let employer;
   let freelancer;
 
   beforeEach(async function () {
     // Deploy the contract
     const FreelancerContract = await ethers.getContractFactory("Freelance");
-    freelancerContract =  (await FreelancerContract.deploy()) as unknown as Freelance
+    freelancerContract = (await FreelancerContract.deploy()) as unknown as Freelance
     await freelancerContract.waitForDeployment();
 
     // Get signers
-    [owner, freelancer] = await ethers.getSigners();
+    [employer, freelancer] = await ethers.getSigners();
   });
 
   it("Should register a freelancer successfully", async function () {
-    const name = "John Doe";
-    const skills = "Web Development";
-    const country = "USA";
-    const gigTitle = "Build a Website";
-    const gigDesc = "I will build a responsive website for your business.";
-    const images = ["image1.png", "image2.png"];
-    const startingPrice = ethers.parseEther("0.1");
+
 
     // Get the freelancer's address
     const freelancerAddress = await freelancer.getAddress();
 
-    // Register the freelancer
+    //  Test: Register the freelancer
     await expect(
       freelancerContract
         .connect(freelancer)
@@ -47,5 +51,22 @@ describe("FreelancerRegistration", function () {
     expect(freelancerInfo.gigTitle).to.equal(gigTitle);
   });
 
-  
+  // Test: Creating a job
+  it("Should create a job", async function () {
+    await freelancerContract
+      .connect(employer)
+      .registerEmployer("Ahmod", "technology", "United States", "https://img.com");
+    await freelancerContract
+      .connect(employer)
+      .createJob(gigTitle, gigDesc, startingPrice);
+
+    const job = await freelancerContract.getJobByID("1");
+    expect(job.employer).to.equal(await employer.getAddress());
+    expect(job.title).to.equal(gigTitle);
+    expect(job.description).to.equal(gigDesc);
+    expect(job.budget).to.equal(startingPrice);
+    expect(job.completed).to.be.false;
+  });
+
+
 });
